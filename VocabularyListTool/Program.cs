8772d73934093738a208f055c74807f9183a3948
Harry <harryxiaxia@gmail.com>
2017-04-08 19:08:08 -0500

    Initially commiting for IdeasZone solution and VocabularyDotCom project

---
+++ VocabularyListTool/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using System.Configuration;
+
+namespace VocabularyListTool
+{
+    class Program
+    {
+        private const string USERNAME = "harryxiaxia@gmail.com";
+        private const string PASSWORD = "775852188120";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var driver = new ChromeDriver();
+            var site = new VocabularyDotCom(driver);
+            var words = new List<string> { "simile", "perfect", "teacher", "student" };
+
+            site.OpenVocabularyDotCom().Login(USERNAME, PASSWORD).AddWordsToList("Test", words);
+            
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ VocabularyListTool/VocabularyDotCom.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace VocabularyListTool
+{
+    class VocabularyDotCom
+    {
+        private IWebDriver driver;
+
+        public VocabularyDotCom(IWebDriver drive)
+        {
+            this.driver = drive;
+        }
+
+        public VocabularyDotCom OpenVocabularyDotCom()
+        {
+            driver.Url = "https://www.vocabulary.com/";
+            return this;
+        }
+
+        public VocabularyDotCom Login(string username, string password)
+        {
+            driver.FindElement(By.ClassName("login")).Click();
+            driver.FindElement(By.Name("username")).Clear();
+            driver.FindElement(By.Name("username")).SendKeys(username);
+
+            driver.FindElement(By.Name("password")).Clear();
+            driver.FindElement(By.Name("password")).SendKeys(password);
+
+            driver.FindElement(By.ClassName("green")).Click();
+            return this;
+        }
+
+        public VocabularyDotCom SelectLists()
+        {
+            driver.FindElement(By.LinkText("LISTS")).Click();
+            return this;
+        }
+
+        public VocabularyDotCom GoToListsPage()
+        {
+            driver.Url = "https://www.vocabulary.com/account/lists";
+            return this;
+        }
+
+        public VocabularyDotCom GoToList(string name)
+        {
+            GoToListsPage();
+            driver.FindElement(By.PartialLinkText(name)).Click();
+            return this;
+        }
+
+        public VocabularyDotCom GoToListAndEdit(string name)
+        {
+            GoToList(name);
+            driver.FindElement(By.PartialLinkText("Edit")).Click();
+            return this;
+        }
+
+        public VocabularyDotCom AddWordsToList(string listName, List<string> words)
+        {
+            GoToList(listName);
+
+            foreach(var word in words)
+            {
+                driver.FindElement(By.PartialLinkText("Edit")).Click();
+                //driver.FindElement(By.ClassName("wordInput")).Clear();
+                //driver.FindElement(By.ClassName("wordInput")).SendKeys(word);
+                driver.FindElement(By.CssSelector("[placeholder='add a word']")).SendKeys(word);
+                driver.FindElement(By.CssSelector("[placeholder='add a word']")).Clear();
+                driver.FindElement(By.ClassName("green")).Click();
+                Thread.Sleep(1 * 1000);
+            }
+            return this;
+        }
+
+    }
+}
